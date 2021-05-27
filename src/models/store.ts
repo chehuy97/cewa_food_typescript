bec7d441e3837673427dede9cae667a64eefb3e8
Quang Huy Che <chehuy97@gmail.com>
2021-05-27 10:56:05 +0700

    add image in store model

--- src/models/store.ts
+++ src/models/store.ts
@@ -5,21 +5,27 @@ export const storeSchema:Schema = new Schema({
     name: String,
     address: String,
     type:String,
-    rating:Number
+    rating:Number,
+    image:{
+        type:String,
+        default:'https://images.foody.vn/res/g27/263004/prof/s1242x600/beauty-upload-api-image-200805140406.jpeg'
+    }
 })
 
 export interface IStore extends Document {
     name:string,
     address:string,
     type:string,
-    rating:string
+    rating:string,
+    image:string
 }
 
 export interface StoreRequest {
     name:string,
     address:string,
     type:string,
-    rating:string
+    rating:string,
+    image:string,
 }
 
 storeSchema.plugin(normalize)
