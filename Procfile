e7f38bf793067161aaad7d12b6206a7054c44449
Quang Huy Che <chehuy97@gmail.com>
2021-05-28 13:49:57 +0700

    try upload thirteen

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run dev
+web: npx nodemon src/index.ts
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ database()
 production(app);
 routes(app);
 
-//const port: string | number = process.env.PORT || 5000;
+const port: string | number = process.env.PORT || 5000;
 
-const server = app.listen(process.env.PORT || 3000, () => {
-   console.log(`Listening on port`);
+const server = app.listen(port, () => {
+   console.log(`Listening on port ${port}`);
 });
 
 module.exports = server;
