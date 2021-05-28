01f4f9c495bac3336caddd8aaf9dfbce8d9a1300
Quang Huy Che <chehuy97@gmail.com>
2021-05-28 10:00:48 +0700

    up code to heroku eighth

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ database()
 production(app);
 routes(app);
 
-const port: string | number = process.env.PORT || 5000;
+const port: string | number = 5000;
 
 const server = app.listen(port, () => {
    console.log(`Listening on port ${port}`);
