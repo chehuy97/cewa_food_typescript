67785840b24a75f7c72fbcf234b2ca7939af8a02
Quang Huy Che <chehuy97@gmail.com>
2021-05-28 10:48:55 +0700

    up code to heroku twelveth

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,15 @@ import {logging, database, routes, production} from './startups';
 import {EnvironmentVariable} from './config'
 
 const app:Express = express();
-
 logging();
 database()
 production(app);
 routes(app);
 
-const port: string | number = EnvironmentVariable.PORT || 5000;
+//const port: string | number = process.env.PORT || 5000;
 
-const server = app.listen(process.env.PORT || 5000, () => {
-   console.log(`Listening on port ${port}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+   console.log(`Listening on port`);
 });
 
 module.exports = server;
